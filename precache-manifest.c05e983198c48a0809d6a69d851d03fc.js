6ce23a63ecfa91b2a64b6177626166a60002e0de
jeffry zaman <jeffry.zaman@gmail.com>
2019-06-28 00:56:10 -0400

    Updates

---
+++ precache-manifest.c05e983198c48a0809d6a69d851d03fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a84fa0dddd98f588c172b0a79b1f6766",
+    "url": "/Grocery/index.html"
+  },
+  {
+    "revision": "a2fbd6add04a1301eb83",
+    "url": "/Grocery/static/css/main.42592963.chunk.css"
+  },
+  {
+    "revision": "270117d60ee695b64010",
+    "url": "/Grocery/static/js/2.1135c22b.chunk.js"
+  },
+  {
+    "revision": "a2fbd6add04a1301eb83",
+    "url": "/Grocery/static/js/main.4bda4101.chunk.js"
+  },
+  {
+    "revision": "39957cb15353336bccda",
+    "url": "/Grocery/static/js/runtime~main.c1f878f7.js"
+  }
+]);
